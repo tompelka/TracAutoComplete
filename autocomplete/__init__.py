dfadd31ae67c43add3b570bc1937b6373d4ec583
Tomas Pelka <tpelka@redhat.com>
2013-03-29 15:25:02 +0100

    clean up logging

--- autocomplete/__init__.py
+++ autocomplete/__init__.py
@@ -4,10 +4,6 @@
 from trac.core import Component, implements
 from trac.ticket.api import ITicketManipulator, ITicketActionController
 from trac.env import Environment
-import logging
-
-myenv = Environment('/srv/trac/desktopqe-backlog-test')
-myenv.log.setLevel(logging.DEBUG)
 
 class FillInTheComplete(Component):
     implements(ITicketManipulator)
@@ -21,12 +17,12 @@ class FillInTheComplete(Component):
         pass
 
     def validate_ticket(self, req, ticket):
-        myenv.log.debug("Req action %s", req.args)
+        self.log.debug("Req action %s", req.args)
         if ticket.exists:
             if req.args['action'] in self._get_close_action():
-                myenv.log.debug("Setting up complete")
+                self.log.debug("Setting up complete")
                 ticket.values['complete'] = '100'
-                myenv.log.debug("Complete = %s", ticket.values['complete'])
+                self.log.debug("Complete = %s", ticket.values['complete'])
         else:
             ticket.values['complete'] = '0'
 
